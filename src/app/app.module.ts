d988fb1c5e8873af131b25d68cc626a09a797904
Anastasiia Santalova <Anastasiia_Santalova@epam.com>
2020-04-14 19:24:06 +0300

    Added custom structural directive, some visual improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClassDirective } from './directives/class/class.directive';
+import { TimesDirective } from './directives/times/times.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClassDirective
+    ClassDirective,
+    TimesDirective
   ],
   imports: [
     BrowserModule,
